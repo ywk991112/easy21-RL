9ef0e7db81aaf34185d2b5337a2b4d9a247acbeb
kwei <ywk991112@gmail.com>
2016-05-13 04:28:22 +0800

    linear function approximation version 1 with error

--- LinearFuncApprox.py
+++ LinearFuncApprox.py
@@ -4,14 +4,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 
-class TDLearning:
+class LinearFuncApprox:
     def __init__(self, env):
         self.QList = defaultdict(float) #(s, a)
         self.EList = defaultdict(float) #(s, a)
-        self.NList = defaultdict(int) #(s, a)
+        self.theta = np.zeros(shape=(3, 6, 2))
         self.env = env
 
-    def Sarsa(self, lamb, iter, gamma):
+    def SarsaLambLinFApp(self, lamb, iter, gamma):
         for i in range(iter):
             self.EList.clear()
             curState = self.env.getInitState()
@@ -20,29 +20,43 @@ class TDLearning:
                 for (s, a) in self.EList:
                     self.EList[(s, a)] = self.EList[(s, a)] * gamma
                 self.EList[(curState, curAction)] += 1
-                self.visit(curState, curAction)
                 nextState, reward = self.env.step(curState, curAction)
                 nextAction = self.epsilonGreedy(nextState)
-                tdError = reward + gamma * self.QList[(nextState, nextAction)] - self.QList[(curState, curAction)]
+                tdError = reward + gamma * self.getValue(nextState, nextAction) - self.getValue(curState, curAction)
+                alpha = 0.01
                 for (s, a) in self.EList:
-                    self.QList[(s, a)] = self.QList[(s, a)] + self.alpha(s, a) * tdError * self.EList[(s, a)]
+                    self.theta += alpha * tdError * (self.EList[(s, a)] * self.getFeature(s, a))
                 curState, curAction = nextState, nextAction
-
-    def reset(self):
-        self.QList.clear()
-        self.EList.clear()
-        self.NList.clear()
 
     def msError(self, realQList):
         error = 0
         for i in range(22):
             for j in range(11):
                 for a in (Action.stick, Action.hit):
-                    error += (self.QList[(i, j), a] - realQList[(i, j), a]) **2
+                    error += (self.getValue((i, j), a) - realQList[(i, j), a]) **2
         return error
 
-    def alpha(self, state, action):
-        return 1. / self.NList[state, action]
+    def getValue(self, s, a):
+        if not s:
+            return 0
+        return np.sum(self.theta * self.getFeature(s, a))  # ?
+
+    def getFeature(self, s, a):
+        phi = np.zeros((3, 6, 2))
+        d_i = [(1, 4), (4, 7), (7, 10)]
+        p_i = [(1, 6), (4, 9), (7, 12), (10, 15), (13, 18), (16, 21)]
+        aIdx = 1 if a == Action.hit else 0
+        for dIdx, d_itv in enumerate(d_i):
+            if not d_itv[0] <= s[1] <= d_itv[1]:
+                continue
+            for pIdx, p_itv in enumerate(p_i):
+                if p_itv[0] <= s[0] <= p_itv[1]:
+                    phi[dIdx][pIdx][aIdx] += 1
+        return phi
+
+    def reset(self):
+        self.QList.clear()
+        self.EList.clear()
 
     def epsilonGreedy(self, s):
         optAction = None
@@ -50,12 +64,9 @@ class TDLearning:
             optAction = Action.stick
         else:
             optAction = Action.hit
-        epsilon = 100. / (100. + (self.NList[s]))
+        epsilon = 0.05
         if random.random() < epsilon:
             return random.choice([Action.hit, Action.stick])
         else:
             return optAction
 
-    def visit(self, s, a):
-        self.NList[s] += 1
-        self.NList[s, a] += 1
--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 from environment import Environment
 from MCLearning import MCLearning
 from TDLearning import TDLearning
+from LinearFuncApprox import LinearFuncApprox
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -18,7 +19,7 @@ tdl = TDLearning(env)
 for lamb in np.arange(0, 1, 0.1):
     tdl.reset()
     tdl.Sarsa(lamb, 1000, 1)
-    print 'lambda =', lamb, ', error =', tdl.error(mcl.QList)
+    print 'lambda =', lamb, ', error =', tdl.msError(mcl.QList)
 
 episodes = np.arange(0, 100000, 10000)
 errLam0 = []
@@ -26,10 +27,41 @@ errLam1 = []
 for epi in episodes:
     tdl.reset()
     tdl.Sarsa(0, epi, 1)
-    errLam0.append(tdl.error(mcl.QList))
+    errLam0.append(tdl.msError(mcl.QList))
     tdl.reset()
     tdl.Sarsa(1, epi, 1)
-    errLam1.append(tdl.error(mcl.QList))
+    errLam1.append(tdl.msError(mcl.QList))
+plt.plot(episodes, errLam0, label="lambda = 0")
+plt.plot(episodes, errLam1, label="lambda = 1")
+plt.legend()
+plt.xlabel('Episodes')
+plt.ylabel('Mean Squared Errors')
+plt.show()
+
+# test linear function approximation
+lfa = LinearFuncApprox(env)
+lambda_a = np.arange(0, 1, 0.1)
+err = []
+for lamb in lambda_a:
+    lfa.reset()
+    lfa.SarsaLambLinFApp(lamb, 1000, 1)
+    err.append(lfa.msError(mcl.QList))
+plt.plot(lambda_a, err)
+plt.legend()
+plt.xlabel('lambda')
+plt.ylabel('Mean Squared Errors')
+plt.show()
+
+episodes = np.arange(0, 100000, 10000)
+errLam0 = []
+errLam1 = []
+for epi in episodes:
+    lfa.reset()
+    lfa.SarsaLambLinFApp(0, epi, 1)
+    errLam0.append(lfa.msError(mcl.QList))
+    lfa.reset()
+    lfa.SarsaLambLinFApp(1, epi, 1)
+    errLam1.append(lfa.msError(mcl.QList))
 plt.plot(episodes, errLam0, label="lambda = 0")
 plt.plot(episodes, errLam1, label="lambda = 1")
 plt.legend()
